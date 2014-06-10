b61ac293fd775bba1087a02e91624f1a075dbf44
Hui Wang <ncepuwanghui@gmail.com>
2014-06-10 15:03:17 +0800

    ch2 example

---
+++ ch2/example/fathm_ft.c
@@ -0,0 +1,23 @@
+/*************************************************************************
+	> File Name: fathm_ft.c
+	> Author: Hui Wang
+	> Mail: ncepuwanghui@gmail.com 
+	> Created Time: Tue 10 Jun 2014 02:54:06 PM CST
+ ************************************************************************/
+/*
+ * 功能：把两个fathoms(深度单位)换算成英尺
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int feet, fathoms;
+
+    fathoms = 2;
+    feet = 6 * fathoms;
+    printf("There are %d feet in %d fathoms!\n", feet, fathoms);
+    printf("Yes, I said %d feet!\n", 6 * fathoms);
+
+    return 0;
+}
---
+++ ch2/example/two_func.c
@@ -0,0 +1,26 @@
+/*************************************************************************
+	> File Name: two_func.c
+	> Author: Hui Wang
+	> Mail: ncepuwanghui@gmail.com 
+	> Created Time: Tue 10 Jun 2014 02:57:17 PM CST
+ ************************************************************************/
+/*
+ * 功能：在一个文件中使用两个函数
+ */
+#include <stdio.h>
+
+void butler(void);//ISO/ANSI C函数原型
+
+int main(void)
+{
+    printf("I will summon  the butler function.\n");
+    butler();
+    printf("Yes. Bring me some tea and writeable CD-ROMS.\n");
+
+    return 0;
+}
+
+void butler(void)
+{
+    printf("You rang, sir?\n");
+}
