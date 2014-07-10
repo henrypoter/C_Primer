0b1d39cafd421171f2fdff034eeb5e2316313a3f
Hui Wang <ncepuwanghui@gmail.com>
2014-07-10 23:59:52 +0800

    ch12 exercises

---
+++ ch12/exercises/12_6_count_frequency_of_random_numbers.c
@@ -0,0 +1,50 @@
+/*************************************************************************
+  > File Name: 12_6_count_frequency_of_random_numbers.c
+  > Author: Hui Wang
+  > Mail: ncepuwanghui@gmail.com 
+  > Created Time: Thu 10 Jul 2014 11:49:13 PM CST
+ ************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <time.h>
+#include <stdbool.h>
+
+void count_frequency(const int num, const int span);
+
+int main(void)
+{
+    const int SPAN = 10;
+    int num = 0;
+
+    printf("Enter the number of random numbers: ");
+    while (scanf("%d", &num) == 1)
+    {
+        count_frequency(num, SPAN);
+        printf("Enter the number of random numbers: ");
+    }
+
+    return 0;
+}
+
+void count_frequency(const int num, const int span)
+{
+    int i = 0;
+    int * temp = (int *) malloc(span * sizeof(int));
+    memset(temp, 0, sizeof(int) * span);
+
+    srand((unsigned int)time(NULL));
+
+    for (i = 0; i < num; i++)
+    {
+        temp[rand() % span]++;
+    }
+
+    for (i = 0; i < span; i++)
+    {
+        printf("%d : %d\n", i, temp[i]);
+    }
+
+    free(temp);
+}
